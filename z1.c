3653065255dc6f00fa39f066e59fbcdbec191501
NEDRI <100300622+NEDRI@users.noreply.github.com>
2026-03-30 16:17:54 +0200

    Create z1.c

---
+++ z1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int a=5;
+  //printf("podaj liczbe \n");
+  //scanf("%d",&a);
+  if (a > 0)
+  {
+  printf("liczba jest dodatnia: %d", a);
+  }
+  return 0;
+}

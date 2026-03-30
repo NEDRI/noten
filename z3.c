af243b7e9aabcc5f61da03717efb8a5e3569091e
NEDRI <100300622+NEDRI@users.noreply.github.com>
2026-03-30 16:31:57 +0200

    Create z3.c

---
+++ z3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int a=4;
+  int b=8;
+  //printf("podaj liczby a i b \n");
+  //scanf("%d",&a);
+  //scanf("%d",&b);
+  if (a > b)
+  {
+  printf("liczba: %d jest wieksza",a);
+  }
+  else
+  {
+  printf("liczba: %d jest wieksza",b);
+  }
+  
+  return 0;
+}
